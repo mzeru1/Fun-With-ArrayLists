28a1d0976ae99c830286972df7f36bb8d223ec96
Meron Zeru <sologolom@gmail.com>
2019-04-25 11:22:27 -0400

    updated loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,30 +6,35 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Scanner scan = new Scanner(System.in);
-        ArrayList<String > colors = new ArrayList<String>();
+        ArrayList<String> colors = new ArrayList<String>();
         colors.add("red");
         colors.add("blue");
         colors.add("green");
-       // System.out.println(colors.get(1));
+        // System.out.println(colors.get(1));
 
-       // String done = " ";
+        // String done = " ";
         String newColor = " ";
         System.out.println("Eneter colort type q when done");
-        while(!newColor.equalsIgnoreCase("q")){
+        while (!newColor.equalsIgnoreCase("q")) {
             newColor = scan.nextLine();
             colors.add(newColor);
-          //  System.out.println("enter another color press q when done?");
-           // done = scan.nextLine();
-           // scan.nextLine();
+            //  System.out.println("enter another color press q when done?");
+            // done = scan.nextLine();
+            // scan.nextLine();
 
         }
         for (int i = 0; i<colors.size()-1; i++){
             System.out.println(colors.get(i));
         }
-        for (String colorList:colors){
-            System.out.println(colorList);
-        }
+
+        // enhanced forloop prefered way
+
+            for (String colorList : colors) {
+                if (!colorList.equalsIgnoreCase("q"))
+                    System.out.println(colorList);
+            }
+
     }
 }
